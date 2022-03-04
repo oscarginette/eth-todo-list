427e00ab4c22f2ec2d96558008cc91fcfe29fc78
oscar.ginette <oscar.ginette@dynatrace.com>
2022-03-04 12:51:20 +0100

    Add struct

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -4,4 +4,12 @@ contract TodoList {
     // Keep track of the numbers of todos
     // State variables similar to class variables
     uint public taskCount = 0;
+
+
+    struct Task {
+        //uint cant be negative
+        uint id;
+        string content;
+        bool completed;
+    }
 }
